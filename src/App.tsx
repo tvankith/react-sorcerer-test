1f7ea4053f74c101fd6883e4c9165418c50aed5b
Ankith T V <tvankith@gmail.com>
2024-03-04 21:46:48 +0530

    fix: basic editor

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CustomEditor from './Editor'
+
+function App() {
+
+  return (
+    <div className='app-container'>
+     <CustomEditor/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Editor.tsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import { Editor, EditorState, Modifier, RichUtils, getDefaultKeyBinding } from 'draft-js';
+import 'draft-js/dist/Draft.css';
+import "./Editor.css";
+
+const colorStyleMap = {
+    red: {
+      color: 'rgba(255, 0, 0, 1.0)',
+    },
+}
+const CustomEditor = () => {
+    const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+
+    const handleChange = (newEditorState: EditorState) => {
+        setEditorState(newEditorState);
+    };
+
+    const handleKeyCommand = (command, editorState) => {
+        const newState = RichUtils.handleKeyCommand(editorState, command);
+
+        if (newState) {
+            handleChange(newState);
+            return 'handled';
+        }
+
+        return 'not-handled';
+    };
+
+    const applyColor = (editorState: EditorState) => {
+
+        // Let's just allow one color at a time. Turn off all active colors.
+        const nextContentState = editorState.getCurrentContent()
+
+        let nextEditorState = EditorState.push(
+            editorState,
+            nextContentState,
+            'change-inline-style'
+        );
+
+
+
+        nextEditorState = RichUtils.toggleInlineStyle(
+            nextEditorState,
+            "red"
+        );
+        return nextEditorState
+    }
+
+    const removeCharacter = (newEditorState: EditorState, string: string)=> {
+        const selectionState = newEditorState.getSelection();
+        const contentState = newEditorState.getCurrentContent();
+        const newContentState = Modifier.removeRange(
+            contentState,
+            selectionState.merge({
+              anchorOffset: 0,
+              focusOffset: string.length,
+            }),
+            'backward'
+          );
+          const newEditorStateWithRemovedChar = EditorState.push(editorState, newContentState, 'remove-range');
+          return newEditorStateWithRemovedChar
+    }
+
+    const keyBindingFn = (e) => {
+        if (e.keyCode === 32) { // Space key
+            const selection = editorState.getSelection();
+            const contentState = editorState.getCurrentContent();
+            const block = contentState.getBlockForKey(selection.getStartKey());
+            const blockText = block.getText();
+
+            const newEditorState = removeCharacter(editorState, blockText)
+            if (blockText === '#') {
+                handleChange(RichUtils.toggleBlockType(
+                    newEditorState,
+                    'header-one'
+                ))
+            }
+            else if (blockText === '*') {
+                handleChange(RichUtils.toggleInlineStyle(newEditorState, 'BOLD'))
+            } else if (blockText === '**') {
+                handleChange(applyColor(newEditorState))
+            } else if (blockText === '***') {
+                handleChange(RichUtils.toggleInlineStyle(newEditorState, 'UNDERLINE'))
+            }
+        }
+        return getDefaultKeyBinding(e);
+    };
+
+    return (
+        <div className='editor-container'>
+            <Editor
+                customStyleMap={colorStyleMap}
+                editorState={editorState}
+                onChange={handleChange}
+                handleKeyCommand={handleKeyCommand}
+                keyBindingFn={keyBindingFn}
+            />
+            <button
+                onClick={() => {
+                    // 
+                }}
+            >Save</button>
+        </div>
+    );
+};
+
+export default CustomEditor;
